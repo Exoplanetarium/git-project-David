30b3e273b9da8049db1f8ee245f413ff39c2c302
Exoplanetarium <89407442+Exoplanetarium@users.noreply.github.com>
2026-09-17 11:13:57 -0700

    (GP-2.1): Program Can Initialize Repository

--- Git.java
+++ Git.java
@@ -1,5 +1,44 @@
+import java.io.File;
+import java.io.IOException;
+
 class Git {
     public static void main(String[] args) {
+        try {
+            initRepo();
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+    public static void initRepo() throws IOException {
+        File git = new File("git");
+        File objects = new File("git/objects");
+
+        boolean doesEverythingExist = true;
         
+        if (git.mkdir() == true) {
+            doesEverythingExist = false;
+        }
+
+        if (objects.mkdir() == true) {
+            doesEverythingExist = false;
+        }
+
+        File index = new File("git/index");
+        File head = new File("git/HEAD");
+
+        if (index.createNewFile() == true) {
+            doesEverythingExist = false;
+        }
+
+        if (head.createNewFile() == true) {
+            doesEverythingExist = false;
+        }
+
+        if (doesEverythingExist == true) {
+            System.out.println("Git Repository Already Exists");
+        } else {
+            System.out.println("Git Repository Created");
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # git-project-David
+
+initRepo() - inits new git repo under git/ with objects/, HEAD, and index inside
