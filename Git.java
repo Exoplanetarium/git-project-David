94efb5ce227b20e940b7cfe73785354c4e406b46
Exoplanetarium <89407442+Exoplanetarium@users.noreply.github.com>
2026-09-22 08:47:52 -0700

    (GP-2.2): Created a Hash Function
    Used the MessageDigest class and SHA-1 to hash the file, taking the file path as input. no helper methods were used.

--- Git.java
+++ Git.java
@@ -1,16 +1,24 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 
 class Git {
     public static void main(String[] args) {
         try {
-            initRepo();
+            init();
+            File test = new File("test.txt");
+            test.createNewFile();
+            System.out.println(hash(test.getPath()));
         } catch (IOException e) {
             return;
-        }
+        }      
     }
 
-    public static void initRepo() throws IOException {
+    public static void init() throws IOException {
         File git = new File("git");
         File objects = new File("git/objects");
 
@@ -39,6 +47,35 @@ class Git {
             System.out.println("Git Repository Already Exists");
         } else {
             System.out.println("Git Repository Created");
+        }
+    }
+
+    public static String hash(String filePath) throws IOException {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String line;
+            StringBuilder text = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append("\n");
+            }
+
+            // deletes last new line
+            if (text.length() > 1) {
+                text.delete(text.length() - 1, text.length());
+            }
+
+            br.close();
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] hash = digest.digest(text.toString().getBytes()); 
+            String hashString = HexFormat.of().formatHex(hash);
+
+            return hashString;
+
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No such algorithm");
+            return "";
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # git-project-David
 
-initRepo() - inits new git repo under git/ with objects/, HEAD, and index inside
+init() - inits new git repo under git/ with objects/, HEAD, and index inside
+hash() - takes file path then hashes it with SHA-1 and returns the hash string in hex
