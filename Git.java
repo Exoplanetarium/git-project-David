44a2e638d8f4378ebc5969e9dac67114ed6b36be
Exoplanetarium <89407442+Exoplanetarium@users.noreply.github.com>
2026-09-24 20:11:12 -0700

    (GP-2.4+): Added support for changed files in index
    + comments to help whoever's going to look at my code

--- Git.java
+++ Git.java
@@ -34,7 +34,7 @@ class Git {
 
         boolean doesEverythingExist = true;
         
-        if (git.mkdir() == true) {
+        if (git.mkdir() == true) { // I can do this since .mkdir() and .createNewFile() return booleans and are idempotent :)
             doesEverythingExist = false;
         }
 
@@ -111,31 +111,46 @@ class Git {
 
     public static void addToIndex(String filePath) {
         try {
-            String entry = hash(filePath) + " " + filePath;
+            String hash = hash(filePath);
+            String entry = hash + " " + filePath;
             
-            FileWriter wr = new FileWriter("git/index", true);
             BufferedReader br = new BufferedReader(new FileReader("git/index"));
-            
-            // checks if entry already exists
             String line;
+            StringBuilder sheet = new StringBuilder();
             while ((line = br.readLine()) != null) {
+                // checks if entry already exists
                 if (line.equals(entry)) {
                     br.close();
-                    wr.close();
                     return;
+                }
+                
+                // checks change in file
+                if (line.split(" ")[1].equals(filePath) && !line.split(" ")[0].equals(hash)) {
+                    sheet.append(entry);
+                    sheet.append("\n");
+                } else {
+                    sheet.append(line);
+                    sheet.append("\n");
                 }
             }
 
             br.close();
 
-            // checks if file is empty
-            if (hash("git/index").equals("da39a3ee5e6b4b0d3255bfef95601890afd80709")) {
-                wr.write(entry);
-            } else {
-                wr.write("\n" + entry);
+            // deletes last new line
+            if (sheet.length() > 0) {
+                sheet.deleteCharAt(sheet.lastIndexOf("\n"));
             }
 
-            wr.close();
+            // checks if file is empty
+            if (hash("git/index").equals("da39a3ee5e6b4b0d3255bfef95601890afd80709")) { // SHA-1 hash for an empty file
+                FileWriter wr = new FileWriter("git/index");
+                wr.write(entry);
+                wr.close();
+            } else {
+                FileWriter wr = new FileWriter("git/index");
+                wr.write(sheet.toString());
+                wr.close();
+            }
 
         } catch (IOException e) {
             return;
